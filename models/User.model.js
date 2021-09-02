ad9039bc249e158d874e17a21e03df130c21310c
Karina Narkeviciute <karina.narkeviciute@gmail.com>
2021-09-02 11:55:05 +0300

    edit password in user model

--- models/User.model.js
+++ models/User.model.js
@@ -1,28 +1,27 @@
 const { Schema, model } = require("mongoose");
 
-const userSchema = new Schema(
-  {
-    username: {
-      type: String,
-      trim: true,
-      required: [true, "Name is required."],
-      // unique: true,
-    },
-    email: {
-      type: String,
-      required: [true, "Email is required."],
-      match: [/^\S+@\S+\.\S+$/, "Please use a valid email address."],
-      unique: true,
-      lowercase: true,
-      trim: true,
-    },
-    password: {
-      type: String,
-      required: [true, "Password is required."],
-    },
-    address: String,
-  }
-);
+const userSchema = new Schema({
+  username: {
+    type: String,
+    trim: true,
+    required: [true, "Name is required."],
+    // unique: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required."],
+    match: [/^\S+@\S+\.\S+$/, "Please use a valid email address."],
+    unique: true,
+    lowercase: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required."],
+    match: [/^[a-zA-Z0-9!@#$%^&*]{6,16}$/, "Please use valid password."],
+  },
+  address: String,
+});
 
 const User = model("User", userSchema);
 
